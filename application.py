0b0684e8e8f5938d3a80159a6a37ce989ac0e68a
Tony Nguyen <tonynguyen0326@gmail.com>
2026-02-09 01:41:20 -0700

    Added login, logout, home page

--- application.py
+++ application.py
@@ -21,11 +21,12 @@ Session(app)
 engine = create_engine(os.getenv("DATABASE_URL"))
 db = scoped_session(sessionmaker(bind=engine))
 
-
+# main page
 @app.route("/")
 def index():
-    return redirect("/register")
+    return render_template("index.html")
 
+# registration page
 @app.route("/register", methods=["GET", "POST"])
 def register():
     if request.method == "POST":
@@ -63,3 +64,34 @@ def register():
     else:
         return render_template("register.html")
     
+# login page
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    if request.method == "POST":
+        # get login data
+        username = request.form.get("username")
+        password = request.form.get("password")   
+    
+        # check user credentials
+        user = db.execute(
+            text("SELECT * FROM users WHERE username = :username"),
+            {"username": username}
+        ).fetchone()
+
+        if not user:
+            return render_template("login.html", error="Username not found.")
+        
+        if not check_password_hash(user.password, password):
+            return render_template("login.html", error="Incorrect Password.")
+    
+        # store user credentials in session
+        session["user_id"] = user.id
+        session["username"] = user.username
+
+    return render_template("login.html")
+
+@app.route("/logout")
+def logout():
+    # remove session data
+    session.clear()
+    return render_template("logout.html")
